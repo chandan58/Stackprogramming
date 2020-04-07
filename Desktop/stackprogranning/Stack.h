a96d16ccdcc72d39ffac543c6a2b8bb9f65b8518
chandan58 <62261460+chandan58@users.noreply.github.com>
2020-04-07 10:27:59 +0200

    created 4th function in task 1

--- Desktop/stackprogranning/Stack.h
+++ Desktop/stackprogranning/Stack.h
@@ -1,6 +1,9 @@
-struct stack* new_stack()
+struct stack* new_stack(unsinged capacity)
 {
     struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
+    stack->capacity = capacity;
+    stack->top = -1;
+    stack->array = (int*)malloc(stack->capacity * sizeof(int));
     return stack;
 }
 
@@ -13,4 +16,12 @@ int top(struct stack* stack)
 int size(struct stack *pt)
 {
     return pt->top +1;
+}
+
+void push(struct Stack* stack, int item)
+{
+    if (isFull(stack))
+        return;
+    stack->array[++stack->top] = item;
+    printf("%d pushed to stack\n", item);
 }
