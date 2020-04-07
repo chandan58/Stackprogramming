ba4cec4b513ffa1afaf0b75d3731f4f7ab99a48c
chandan58 <62261460+chandan58@users.noreply.github.com>
2020-04-07 10:18:11 +0200

    created 2nd function in task 1

--- Desktop/stackprogranning/Stack.h
+++ Desktop/stackprogranning/Stack.h
@@ -3,3 +3,9 @@ struct stack* new_stack()
     struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
     return stack;
 }
+
+int top(struct stack* stack)
+{
+
+    return stack->top;
+}
