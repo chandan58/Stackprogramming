b6a7f1d8b2b8d1e454b092b6530c1ee7140ccad4
chandan58 <62261460+chandan58@users.noreply.github.com>
2020-04-07 10:14:34 +0200

    created 1st function in task 1

---
+++ Desktop/stackprogranning/Stack.h
@@ -0,0 +1,5 @@
+struct stack* new_stack()
+{
+    struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
+    return stack;
+}
---
+++ Desktop/stackprogranning/Stack_exam.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include ”stack.h”
+#def ine STACK_SIZE 10
+// S t r u c t u r e d e f i n i n g a s t a c k
+struct stack
+{
+char elements[STACK_SIZE ] ;
+int top ;
+} ;
+int main ( )
+{
+struct stack* st=new_stack ( ) ;
+push ( ’H ’ , i ) ;
+push ( ’ e ’ , i ) ;
+push ( ’ l ’ , i ) ;
+push ( ’ l ’ , i ) ;
+push ( ’ o ’ , i ) ;
+print( st ) ;
+reverse_print ( s t ) ;
+}
